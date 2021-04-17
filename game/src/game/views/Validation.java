42d18dae2229323166dee6d93958369af18e07a0
MasaGt <MasaGt@users.noreply.github.com>
2021-04-17 16:42:34 +1200

    create validation interface

---
+++ game/src/game/views/Validation.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.views;
+
+/**
+ *
+ * @author Masaomi
+ */
+public interface Validation {
+    boolean validate(String input);
+    boolean validate(int input);
+}
