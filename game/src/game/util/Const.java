391296285f80cfcd54b3ac139c18597d469f3b92
MasaGt <MasaGt@users.noreply.github.com>
2021-04-22 21:21:45 +1200

    fix delimiter to /

--- game/src/game/util/Const.java
+++ game/src/game/util/Const.java
@@ -14,7 +14,7 @@ public class Const {
     public static final int ANSWER_QUIZ = 1;
     public static final int USE_LIFELINE = 2;
     public static final int QUIT = 3;
-    public static final String DELIMITER = ",";
+    public static final String DELIMITER = "/";
     public static final int RANKING_RECORD_LIMIT = 10;
     public static final int THE_NUMBER_OF_QUIZ_PER_LEVEL = FINAL_ROUND / Level.values().length;
 }
