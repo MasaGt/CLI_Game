94c964b10bcb05236b07f442df53ba640f7c097c
MasaGt <MasaGt@users.noreply.github.com>
2021-04-22 19:25:54 +1200

    fix the audience bug [0001]

--- game/src/game/entity/Audience.java
+++ game/src/game/entity/Audience.java
@@ -10,6 +10,7 @@ import java.util.Random;
 
 /**
  * Audiemce object.
+ *
  * @author Masaomi
  */
 public class Audience extends Lifeline {
@@ -19,18 +20,17 @@ public class Audience extends Lifeline {
     }
 
     /**
-     * Show the result of poll.
-     * The highest percentage should be for the correct answer.
-     * Totla parcentage should be 100%.
-     * @param quiz 
+     * Show the result of poll. The highest percentage should be for the correct
+     * answer. Totla parcentage should be 100%.
+     * @param quiz
      */
     @Override
     public void execute(QuizInfo quiz) {
-        
+
         if (!this.getIsAvailable()) {
             return;
         }
-        
+
         Random rand = new Random();
 
         List<OptionDto> options = quiz.getOption();
@@ -45,14 +45,23 @@ public class Audience extends Lifeline {
         for (int i = 0; i < options.size(); i++) {
             //index is for diaply purpose.
             int index = i + 1;
-            
+
             //check which otion is the correct answer
             if (quiz.getQuiz().getAnswer() == options.get(i).getId()) {
                 System.out.println(index + ": " + randForCorrect + "%");
+                randForCorrect = 0;
             } else {
-                
-                if (i == options.size() - 1) {
-                    //allocate the rest of parcentage to the last option so that % will be 100% in totoal.
+                if (i == options.size() - 1 || randForCorrect != 0) {
+                    //allocate the rest of parcentage to the last wrong option so that % will be 100% in totoal.
+                    /*
+                    * if the condition is only (i == options.size() - 1).
+                    * It will cause a following bug.
+                    * If the correct option is the last option, i == options.size() - 1 will always return fasle.
+                    *
+                    * e.g. options are option1, option2, option3, option4 and correct option is option4
+                    * the left percentage should be allocated to option3.
+                    * However, optioin3 fall in else scope because option3 is not the last option.
+                    */
                     System.out.println(index + ": " + left + "%");
                 } else {
                     System.out.println(index + ": " + randForOther + "%");
@@ -62,5 +71,4 @@ public class Audience extends Lifeline {
             }
         }
     }
-
 }
